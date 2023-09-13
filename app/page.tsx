2a38c34b528939df09f3e4c1366b507c0af21298
SashaBayan <sashabayan@gmail.com>
2023-09-13 13:36:39 -0700

    add top section
    still working on getting the fonts correct

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
-import Image from "next/image";
 import { NavBar } from "./components/NavBar";
 
 export default function Home() {
   return (
-    <main className="bg-black">
+    <main className="bg-black text-white">
       <NavBar />
+      {/* Top section */}
+      <div className="flex w-full items-center justify-center p-20 ">
+        <div className="flex max-w-xl flex-col gap-10 text-center">
+          <h1 className="font-display text-4xl  font-bold">
+            Helvetica Lake is a software consulting firm specializing in
+            innovative solutions for your digital product needs.
+          </h1>
+          <h3 className="text-base">
+            Frontend development, intricate backend systems, conceptual product
+            development – we got you covered.
+          </h3>
+          {/* CTA */}
+          <a>
+            <button className="rounded border border-solid border-white p-2 px-3.5 text-base">
+              Explore Our Services
+            </button>
+          </a>
+        </div>
+      </div>
     </main>
   );
 }
