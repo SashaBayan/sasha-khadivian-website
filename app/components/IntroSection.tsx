622fcf80f6e564d5716a0d2357464d4af3a38711
SashaBayan <sashabayan@gmail.com>
2023-09-13 14:25:54 -0700

    add responsiveness to font sizes, fix line-height
    Looks like the `text-{}` tailwind classes have a line-height that overwrites the “leading-{}” classes. Adding `!` fixes

--- app/components/IntroSection.tsx
+++ app/components/IntroSection.tsx
@@ -3,17 +3,17 @@ export function IntroSection() {
     // Top section
     <section className="flex w-full items-center justify-center p-20 ">
       <div className="flex max-w-xl flex-col gap-10 text-center">
-        <h1 className="font-primary text-4xl font-bold leading-normal">
+        <h1 className="font-primary text-2xl font-bold !leading-normal sm:text-3xl md:text-4xl">
           Helvetica Lake is a software consulting firm specializing in
           innovative solutions for your digital product needs.
         </h1>
-        <h3 className="font-secondary text-base font-normal leading-relaxed">
+        <h3 className="font-secondary text-sm font-normal !leading-relaxed sm:text-base">
           Frontend development, intricate backend systems, conceptual product
           development – we got you covered.
         </h3>
         {/* CTA */}
         <a>
-          <button className="rounded border border-solid border-white p-2 px-3.5 text-base">
+          <button className="rounded border border-solid border-white p-2 px-3.5 text-sm sm:text-base">
             Explore Our Services
           </button>
         </a>
