3f7607adc10c799e8ad035515ccda603d4aeac25
SashaBayan <sashabayan@gmail.com>
2023-09-16 00:04:50 -0700

    ‘ -> &apos;

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -10,10 +10,10 @@ export function AboutSection() {
         Hello,
       </h3>
       <p className="font-secondary max-w-md text-sm font-light !leading-relaxed md:max-w-lg md:text-base">
-        I'm a highly adaptable and versatile technical leader with a passion for
-        crafting elegant and efficient solutions to complex problems. I have
-        built and maintained software applications and have led teams to do the
-        same.
+        I&apos;m a highly adaptable and versatile technical leader with a
+        passion for crafting elegant and efficient solutions to complex
+        problems. I have built and maintained software applications and have led
+        teams to do the same.
       </p>
       <p className="font-secondary max-w-md text-sm font-light !leading-relaxed md:max-w-lg md:text-base">
         I enjoy mentoring and guiding fellow engineers, helping to increase team
@@ -23,10 +23,10 @@ export function AboutSection() {
       </p>
       <p className="font-secondary max-w-md text-sm font-light !leading-relaxed md:max-w-lg md:text-base">
         I strive to adapt and contribute effectively to each unique environment,
-        always aiming to collaborate and help teams achieve their goals.I am
-        always eager to take on new challenges and continue learning. If you'd
-        like to connect, collaborate or discuss opportunities, please feel free
-        to reach out. Let's create something amazing together!
+        always aiming to collaborate and help teams achieve their goals. I am
+        always eager to take on new challenges and continue learning. If
+        you&apos;d like to connect, collaborate or discuss opportunities, please
+        feel free to reach out. Let&apos;s create something amazing together!
       </p>
       <Image
         width={60}
