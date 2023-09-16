d4042c988e8ceb8e3023e7ae78e8d9791e6d41ef
SashaBayan <sashabayan@gmail.com>
2023-09-16 00:18:27 -0700

    change text color of ContactUs, change icon fills

--- app/components/ContactUsSection.tsx
+++ app/components/ContactUsSection.tsx
@@ -4,14 +4,14 @@ export function ContactUsSection() {
   return (
     <section
       id="contact"
-      className="flex flex-col items-center justify-center gap-10 px-10 py-20 text-center lg:flex-nowrap"
+      className="flex flex-col items-center justify-center gap-10 px-10 py-20 text-center text-[#21313C] lg:flex-nowrap"
     >
       {/* copy */}
       <div className="flex flex-col gap-5">
-        <h3 className="font-primary text-2xl font-bold !leading-normal sm:text-3xl md:text-4xl">
+        <h3 className="font-primary text-2xl font-light !leading-normal sm:text-3xl md:text-4xl">
           Reach Out
         </h3>
-        <p className="max-w-lg font-secondary text-sm font-normal !leading-relaxed sm:text-base">
+        <p className="font-secondary max-w-lg text-sm font-normal !leading-relaxed sm:text-base">
           If you&apos;d like a consultation or have any questions, drop me an
           email. Let&apos;s build something exceptional together.
         </p>
@@ -21,8 +21,8 @@ export function ContactUsSection() {
         <div className="flex items-center justify-center gap-2 hover:animate-pulse">
           <Image
             className="-mb-0.5"
-            width={20}
-            height={20}
+            width={24}
+            height={24}
             src="/mail-icon.svg"
             alt="email icon"
           />
@@ -31,8 +31,8 @@ export function ContactUsSection() {
         <div className="flex items-center justify-center gap-2 hover:animate-pulse">
           <Image
             className="-mt-1"
-            width={34}
-            height={32}
+            width={24}
+            height={24}
             src="/linkedin-icon.svg"
             alt="linkedin icon"
           />
@@ -46,6 +46,7 @@ export function ContactUsSection() {
         </div>
         <div className="flex items-center justify-center gap-2 hover:animate-pulse">
           <Image
+            className="-mt-1"
             width={24}
             height={24}
             src="/calendar-icon.svg"
