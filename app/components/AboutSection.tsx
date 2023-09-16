8b501ef6938a4a5f56286814d42b46f80141ca19
SashaBayan <sashabayan@gmail.com>
2023-09-15 22:37:11 -0700

    update About section, add favicon

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -4,45 +4,36 @@ export function AboutSection() {
   return (
     <section
       id="about"
-      className="flex flex-wrap justify-center gap-20 bg-[#2B3851] px-10 py-20 md:justify-between md:px-16 lg:flex-nowrap lg:gap-32 "
+      className="flex flex-col flex-wrap items-center justify-center gap-10 bg-[#2B5148] px-10 py-20 md:px-16 "
     >
-      {/* copy */}
-      <div className="flex flex-col gap-5">
-        <h3 className="font-primary text-lg font-bold uppercase tracking-widest text-white">
-          About
-        </h3>
-        <p className="max-w-md font-secondary text-sm !leading-relaxed md:max-w-lg md:text-base">
-          At Helvetica Lake, we believe in the power of innovation and efficient
-          software design. Founded by Sasha Khadivian, an engineer with over a
-          decade of experience at companies like Airbnb and Apple, our mission
-          is to help businesses unlock their full potential through tailored
-          software solutions. With deep expertise in various languages and
-          tools, and a passion for driving projects to completion, we are
-          committed to ensuring your software needs are met with precision and
-          dedication.
-        </p>
-      </div>
-      {/* photo */}
-      <div className="flex flex-col items-center justify-center gap-5 lg:w-[448px]">
-        <Image
-          className="hidden rounded-full lg:block"
-          width={160}
-          height={160}
-          src="/founder-photo.png"
-          alt="Sasha Khadivian photo"
-        />
-        <Image
-          className="rounded-full lg:hidden"
-          width={120}
-          height={120}
-          src="/founder-photo.png"
-          alt="Sasha Khadivian photo"
-        />
-        <div className="flex flex-col gap-2 text-center text-sm">
-          <p> Sasha Khadivian </p>
-          <p> Founder of Helvetica Lake</p>
-        </div>
-      </div>
+      <h3 className="font-primary text-lg font-bold uppercase tracking-widest text-white">
+        Hello,
+      </h3>
+      <p className="max-w-md font-secondary text-sm !leading-relaxed md:max-w-lg md:text-base">
+        I'm a highly adaptable and versatile technical leader with a passion for
+        crafting elegant and efficient solutions to complex problems. I have
+        built and maintained software applications and have led teams to do the
+        same.
+      </p>
+      <p className="max-w-md font-secondary text-sm !leading-relaxed md:max-w-lg md:text-base">
+        I enjoy mentoring and guiding fellow engineers, helping to increase team
+        velocity and deliver high-quality products. My experience ranges from
+        startups to FAANG companies, where I have had the opportunity to learn
+        and grow as a professional in various settings.
+      </p>
+      <p className="max-w-md font-secondary text-sm !leading-relaxed md:max-w-lg md:text-base">
+        I strive to adapt and contribute effectively to each unique environment,
+        always aiming to collaborate and help teams achieve their goals.I am
+        always eager to take on new challenges and continue learning. If you'd
+        like to connect, collaborate or discuss opportunities, please feel free
+        to reach out. Let's create something amazing together!
+      </p>
+      <Image
+        width={48}
+        height={48}
+        src="/sasha-teardrop.png"
+        alt="sasha photo"
+      />
     </section>
   );
 }
