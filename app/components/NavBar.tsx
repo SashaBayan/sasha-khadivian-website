8ff3da088800d1f6c482c439fd775fc4743c26de
SashaBayan <sashabayan@gmail.com>
2023-09-29 13:43:36 -0700

    adjust padding of name at top left

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function NavBar() {
   return (
-    <div className="flex w-full justify-between px-10 py-5 text-sm text-[#21313C]">
+    <div className="flex w-full justify-between px-5 py-5 text-sm text-[#21313C]">
       <div>Sasha Khadivian</div>
     </div>
   );
